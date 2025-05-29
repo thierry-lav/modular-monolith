dfaaba46459b5a03bc8a1dc713a40d75afd16542
thierry <lav.thierry@gmail.com>
2025-05-29 13:02:55 +0200

    :sparkles: Init repository

---
+++ src/main/java/com/modularization/monolith/ModularMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.modularization.monolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModularMonolithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModularMonolithApplication.class, args);
+    }
+
+}
